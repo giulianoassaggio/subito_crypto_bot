e48d9061740c3af23ee74a4c0a7b1dfee8cc104c
Giuliano Assaggio <124878741+giulianoassaggio@users.noreply.github.com>
2023-03-25 13:07:53 +0000

    update
    fixato due tre cosucce: tolto il lower() dall'inoltro del messaggio, 
    aggiunto "servizio" e "cerco" al caso immagini e aggiunto il lower() contestualmente in tutte le varie condizioni

--- src/BOT.py
+++ src/BOT.py
@@ -25,24 +25,24 @@ async def gestione_messaggi(update: Update, context: ContextTypes.DEFAULT_TYPE):
         # che serve a eliminare le parole #vendo etc, ma ho pensato di toglierlo perché è bene distinguere vendo da cerco
 
         if user != "Telegram":
-            if mex.photo and "#vendo" in str(mex.caption).lower():
+            if mex.photo and (("#vendo" in str(mex.caption).lower()) or ("#cerco" in str(mex.caption).lower()) or ("#servizio" in str(mex.caption).lower())):
                 # annunci contenenti un immagine: solo la prima viene inviata.
                 # questi annunci vanno inviati in vetrina, poi eliminati gli originali
-                description = str(mex.caption).lower() #estrazione dell'annuncio dalla didascalia
+                description = str(mex.caption) #estrazione dell'annuncio dalla didascalia
                 text_to_be_sent = description + "\n\nANNUNCIO DI:\n" + user
                 photo = mex.photo[-1]           # estrazione della foto dal messaggio
                 photo_file_id = photo.file_id   # id della foto, presente direttamente sui server telegram
                 await context.bot.send_photo(chat_id=CHAT_ID_VETRINA, photo=photo_file_id, caption=text_to_be_sent)
                 await mex.delete()
 
-            elif ("#vendo" in mex.text) or ("#cerco" in mex.text) or ("#servizio" in mex.text):
+            elif ("#vendo" in mex.text.lower()) or ("#cerco" in mex.text.lower()) or ("#servizio" in mex.text.lower()):
                 # annunci da inviare in vetrina, funzionamento analogo alle immagini
                 sale_advertisement = mex.text
                 attached_message = sale_advertisement + "\n\nANNUNCIO DI:\n" + user
                 await context.bot.send_message(CHAT_ID_VETRINA, attached_message)
                 await mex.delete()
             
-            elif "#feedback" in mex.text:
+            elif "#feedback" in mex.text.lower():
                 # feedback da inviare nell'omonimo canale, funzionamento analogo alle immagini
                 feedback_text = mex.text
                 feedback_message = feedback_text + "\n\nFEEDBACK DA PARTE DI:\n" + user
