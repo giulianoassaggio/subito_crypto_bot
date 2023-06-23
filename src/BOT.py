2c361f9c676d6ecdaca09235235d132b0cf2b44e
Giuliano Assaggio <124878741+giulianoassaggio@users.noreply.github.com>
2023-06-23 20:23:33 +0000

    Update BOT.py

--- src/BOT.py
+++ src/BOT.py
@@ -9,17 +9,21 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-CHAT_ID_GRUPPO = -1001397315169    # subito_crypto
-CHAT_ID_VETRINA = -1001872734100    # subito_crypto_vetrina
-CHAT_ID_FEEDBACK = -1001956565384   # subito_crypto_feedbacks
+CHAT_ID_GRUPPO      = -1001397315169    # subito_crypto
+CHAT_ID_VETRINA     = -1001872734100    # subito_crypto_vetrina
+CHAT_ID_FEEDBACK    = -1001956565384    # subito_crypto_feedbacks
+CHAT_ID_NOLEGGI     = -1001644892959    # chat_noleggintermediazionepostale
+CHAT_ID_ASTE        = -1001880758375    # asteinbitcoin (gruppo collegato)
 
 TOKEN_DEL_BOT = "Nascosto su github"
 
 async def gestione_messaggi(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat = update.effective_chat.id     # chat di provenienza dell'annuncio (si suppone sia il mercatino)
+    
+    mex = update.effective_message  # messaggio con l'annuncio
+    user = mex.from_user.name       # autore dell'annuncio
+
     if chat == CHAT_ID_GRUPPO or chat == CHAT_ID_FEEDBACK or chat == CHAT_ID_VETRINA:
-        mex = update.effective_message  # messaggio con l'annuncio
-        user = mex.from_user.name       # autore dell'annuncio
 
         # in tutte le seguenti casistiche, nel messaggio da inviare si piò aggiungere il metodo .split(" ", 1)[1], 
         # che serve a eliminare le parole #vendo etc, ma ho pensato di toglierlo perché è bene distinguere vendo da cerco
@@ -35,19 +39,36 @@ async def gestione_messaggi(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 await context.bot.send_photo(chat_id=CHAT_ID_VETRINA, photo=photo_file_id, caption=text_to_be_sent)
                 await mex.delete()
 
-            elif ("#vendo" in mex.text.lower()) or ("#cerco" in mex.text.lower()) or ("#servizio" in mex.text.lower()):
-                # annunci da inviare in vetrina, funzionamento analogo alle immagini
-                sale_advertisement = mex.text
-                attached_message = sale_advertisement + "\n\nANNUNCIO DI:\n" + user
-                await context.bot.send_message(CHAT_ID_VETRINA, attached_message)
-                await mex.delete()
-            
-            elif "#feedback" in mex.text.lower():
-                # feedback da inviare nell'omonimo canale, funzionamento analogo alle immagini
-                feedback_text = mex.text
-                feedback_message = feedback_text + "\n\nFEEDBACK DA PARTE DI:\n" + user
-                await context.bot.send_message(CHAT_ID_FEEDBACK, feedback_message)
-                await mex.delete()
+        elif ("#vendo" in mex.text.lower()) or ("#cerco" in mex.text.lower()) or ("#servizio" in mex.text.lower()):
+            # annunci da inviare in vetrina, funzionamento analogo alle immagini
+            sale_advertisement = mex.text
+            attached_message = sale_advertisement + "\n\nANNUNCIO DI:\n" + user
+            await context.bot.send_message(CHAT_ID_VETRINA, attached_message)
+            await mex.delete()
+        
+        elif "#feedback" in mex.text.lower():
+            # feedback da inviare nell'omonimo canale, funzionamento analogo alle immagini
+            feedback_text = mex.text
+            feedback_message = feedback_text + "\n\nFEEDBACK DA PARTE DI:\n" + user
+            await context.bot.send_message(CHAT_ID_FEEDBACK, feedback_message)
+            await mex.delete()
+
+    elif chat == CHAT_ID_NOLEGGI:
+        if user != "Telegram" and "#feedback" in mex.text.lower():
+            #esattamente come i feedback sopra, solo provenienti dalla chat noleggi
+            feedback_text = mex.text
+            feedback_message = "(#noleggio)\n\n"+feedback_text + "\n\nFEEDBACK DA PARTE DI:\n" + user
+            await context.bot.send_message(CHAT_ID_FEEDBACK, feedback_message)
+            await mex.delete()
+
+    elif chat == CHAT_ID_ASTE:
+        if user != "Telegram" and "#feedback" in mex.text.lower():
+            #esattamente come i feedback sopra, solo provenienti dalla chat asteinbitcoin
+            feedback_text = mex.text
+            feedback_message = "(#asta)\n\n"+feedback_text + "\n\nFEEDBACK DA PARTE DI:\n" + user
+            await context.bot.send_message(CHAT_ID_FEEDBACK, feedback_message)
+            await mex.delete()
+        
     else:
         # se il messaggio arriva da una chat che non c'entra nulla col mercatino (privato compreso)
         # ad ogni messaggio viene inviato l'errore (nella speranza che il bot venga tolto ahahah)
